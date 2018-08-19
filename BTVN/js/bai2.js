69f30c78f0318bde3496c4d00bad8bb97bfa2d57
mysuong <mysuong210497@gmail.com>
2018-08-19 09:44:21 +0700

    nopbai

--- BTVN/js/bai2.js
+++ BTVN/js/bai2.js
@@ -1,12 +1,11 @@
-$('#accost').click(function(){
+$('#adjacent').click(function(){
 	  
 		$('#prince').animate({left:'450px'},2000);
     	$('#princess').animate({right:'250px'},2000);
 	
 });
-$('#flower').click(function(){
+
+$('#click').click(function(){
 	  
-		$('.boy').attr('src','img/hoa.jpg');
-	
-	
+		$('#flower').show();
 });
